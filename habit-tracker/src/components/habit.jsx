64885e678662b517daa89b531bd50523442851ba
CheeseBall <2489ckckck@naver.com>
2022-07-03 22:46:03 +0900

    a

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -16,16 +16,16 @@ class Habit extends Component {
 	render() {
 		const { name, count } = this.props.habit;
 		return (
-			<li className='habit'>
-				<span className='habit-name'>{name}</span>
-				<span className='habit-count'>{count}</span>
-				<button className='habit-button habit-increase' onClick={this.handleIncrement}>
+			<li className="habit">
+				<span className="habit-name">{name}</span>
+				<span className="habit-count">{count}</span>
+				<button className="habit-button habit-increase" onClick={this.handleIncrement}>
 					<i className="fa-solid fa-square-plus"></i>
 				</button>
-				<button className='habit-button habit-decrease' onClick={this.handleDecrement}>
+				<button className="habit-button habit-decrease" onClick={this.handleDecrement}>
 					<i className="fa-solid fa-square-minus"></i>
 				</button>
-				<button className='habit-button habit-delete' onClick={this.handleDelete}>
+				<button className="habit-button habit-delete" onClick={this.handleDelete}>
 					<i className="fa-solid fa-trash"></i>
 				</button>
 			</li>
