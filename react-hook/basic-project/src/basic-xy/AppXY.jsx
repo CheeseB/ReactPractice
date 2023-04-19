7912bf5396274938b1ed670897d95762ca81af81
CheeseB <2489ckckck@naver.com>
2023-04-19 17:13:28 +0900

    Update AppXY.jsx

--- react-hook/basic-project/src/basic-xy/AppXY.jsx
+++ react-hook/basic-project/src/basic-xy/AppXY.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
   return (
-    <div className="container">
-      <div className="pointer" />
+    <div
+      className="container"
+      onPointerMove={e => {
+        setPosition(prev => ({ ...prev, x: e.clientX }));
+      }}
+    >
+      <div
+        className="pointer"
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+      />
     </div>
   );
 }
