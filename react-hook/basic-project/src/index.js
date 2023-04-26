7e2821b35e1cfd9a1332265526348b84556c9747
CheeseB <2489ckckck@naver.com>
2023-04-26 17:38:46 +0900

    리액트에서 form 사용법

--- react-hook/basic-project/src/index.js
+++ react-hook/basic-project/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppMentorsImmer from './basic-immer/AppMentorsImmer';
+import AppForm from './basic-form/AppForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentorsImmer />
+    <AppForm />
   </React.StrictMode>,
 );
